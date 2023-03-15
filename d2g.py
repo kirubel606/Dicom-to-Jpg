a24699266d2f4e358b0e57fe12264579af790224
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2023-03-14 23:12:48 -0700

    Update d2g.py

--- d2g.py
+++ d2g.py
@@ -1,7 +1,7 @@
 import dicom2jpg
 
-dicom_dir = "dcm"
-export_location = "jpg"
+dicom_dir = "/dcm" #insert your directory here
+export_location = "/jpg" #insert the directory you want your jpg to be in  
 if __name__ == '__main__':
-    dicom2jpg.dicom2jpg(dicom_dir, target_root=export_location)  
+    dicom2jpg.dicom2jpg(dicom_dir, target_root=export_location)  #converts all dicom to jpg and moves to specified export location
 
